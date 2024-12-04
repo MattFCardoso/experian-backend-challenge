8a11cfcf0be3a662036b1c3d37583918b2186d96
MattFC <matheus.fcardoso@hotmail.com>
2024-12-04 08:19:24 -0300

    [config] removing lombok and adding getters and setters

--- src/main/java/com/experian/challenge/model/dto/DeviceResponseDTO.java
+++ src/main/java/com/experian/challenge/model/dto/DeviceResponseDTO.java
@@ -2,7 +2,6 @@ package com.experian.challenge.model.dto;
 
 import lombok.Data;
 
-@Data
 public class DeviceResponseDTO {
     private String deviceId;
     private String osName;
@@ -10,5 +9,53 @@ public class DeviceResponseDTO {
     private String browserName;
     private String browserVersion;
     private int hitCount;
+
+    public String getDeviceId() {
+        return deviceId;
+    }
+
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+
+    public String getOsName() {
+        return osName;
+    }
+
+    public void setOsName(String osName) {
+        this.osName = osName;
+    }
+
+    public String getOsVersion() {
+        return osVersion;
+    }
+
+    public void setOsVersion(String osVersion) {
+        this.osVersion = osVersion;
+    }
+
+    public String getBrowserName() {
+        return browserName;
+    }
+
+    public void setBrowserName(String browserName) {
+        this.browserName = browserName;
+    }
+
+    public String getBrowserVersion() {
+        return browserVersion;
+    }
+
+    public void setBrowserVersion(String browserVersion) {
+        this.browserVersion = browserVersion;
+    }
+
+    public int getHitCount() {
+        return hitCount;
+    }
+
+    public void setHitCount(int hitCount) {
+        this.hitCount = hitCount;
+    }
 }
 
